c5579497b3e1bb4b0d56029abba4c0aa885ef769
MariusSuessmilch <marius.suessmilch@gmail.com>
2025-06-24 14:35:43 +0200

    cleaned up comments

--- app.py
+++ app.py
@@ -2,15 +2,13 @@ import streamlit as st
 from gpt_helper import chat_gpt
 
 st.title("Hi")
-import streamlit as st
-from gpt_helper import chat_gpt
 
-st.title("Hi")
-
-# Probiere folgende Funktionen aus: st.Button, st.text_input, st.write
+# Probiere folgende Funktionen aus: st.title, st.write, st.button, st.text_input
 # Hier findest du sie beschrieben: https://docs.streamlit.io/develop/api-reference
-st.write(chat_gpt("Hello", "You are a helpful assistant."))
+#st.write(chat_gpt(nutzer_prompt, system_prompt))
 
-if st.button("Click me"):
-    st.write("Hello")
+# Dein Code:
+
+
+
     
